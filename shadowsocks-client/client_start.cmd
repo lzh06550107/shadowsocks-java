0894154570208df1a9fe518ff9abe0a8ab2e4c9a
liao06550107 <liao06550107@gmail.com>
2019-11-01 12:33:53 +0800

    修改配置

--- shadowsocks-client/client_start.cmd
+++ shadowsocks-client/client_start.cmd
@@ -1,2 +1,2 @@
 @echo off
-java -jar ./target/shadowsocks-client-1.0.0.jar -l 1081 -s 127.0.0.1 -P 8080 -m aes-128-cfb -p 123456
+java -jar ./target/shadowsocks-client-2.0.0.jar -l 1081 -s 127.0.0.1 -P 8080 -m aes-128-cfb -p 123456
